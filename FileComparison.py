e3238190e98b3e91798be2f92814fe3b19be3b2f
Karthik Subramanian <karthik.subramaniam@viacomcontractor.com>
2018-02-06 22:27:32 -0500

    changed export file from xls to xlsx

--- FileComparison.py
+++ FileComparison.py
@@ -1,13 +1,22 @@
 # This program compares 2 folders and spits out an excel with the results
 
 import Tkinter,tkFileDialog
-import filecmp
 import os
 import hashlib
-import optparse
-import os.path
 import sys
-import xlwt
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Font,colors
+
+book = Workbook()
+
+font = Font(name='Calibri',
+size=11,
+bold=True,
+italic=False,
+vertAlign=None,
+underline='none',
+strike=False,
+color=colors.RED)
 
 # function to calculate the checksum value of the provided file path
 def md5Checksum(filePath):
@@ -30,39 +39,35 @@ folder_size_destination = 0
 
 # Selects the Source Folder
 sourceFolder = tkFileDialog.askdirectory()
+# sourceFolder = 'C:\Users\subramak\Downloads\Test'
+
 print('Source Path: ' + sourceFolder)
 print('')
 
 # Selects the Destination Folder
 destinationFolder = tkFileDialog.askdirectory()
+# destinationFolder = 'C:\Users\subramak\Downloads\Test - Copy'
+
 print('Destination Path: ' + destinationFolder)
 print('')
    
-# open a workbook to save the results   
-book = xlwt.Workbook(encoding="utf-8")
 
-sheet1 = book.add_sheet("Results")
-style0 = xlwt.easyxf('font: name Times New Roman, color-index red, bold on')
-sheet1.col(0).width = 256 * 70
-sheet1.col(1).width = 256 * 50
-sheet1.col(2).width = 256 * 70
-sheet1.col(3).width = 256 * 50
-sheet1.col(4).width = 256 * 70
+sheet1 = book.create_sheet('Results')
 
-sheet1.write(0,0,'Source Folder: ',style0)
+sheet1.cell(1,1,'Source Folder: ')
 
-sheet1.write(0,1, sourceFolder)
+sheet1.cell(1,2, sourceFolder)
 
-sheet1.write(1,0,'Destination Folder:',style0)
-sheet1.write(1,1, destinationFolder)
+sheet1.cell(2,1,'Destination Folder:')
+sheet1.cell(2,2, destinationFolder)
 
-sheet1.write(5,0,'File Comparison Results:',style0)
+sheet1.cell(5,1,'File Comparison Results:')
 
-sheet1.write(7,0,'Source File Path',style0)
-sheet1.write(7,1,'Source File MD5',style0)
-sheet1.write(7,2,'Destination File Path',style0)
-sheet1.write(7,3,'Destination File MD5',style0)
-sheet1.write(7,4,'Result',style0)
+sheet1.cell(7,1,'Source File Path')
+sheet1.cell(7,2,'Source File MD5')
+sheet1.cell(7,3,'Destination File Path')
+sheet1.cell(7,4,'Destination File MD5')
+sheet1.cell(7,5,'Result')
 
 i = 8
 
@@ -70,7 +75,7 @@ i = 8
 for (path, dirs, files) in os.walk(sourceFolder):
     for file in files:
         source_filename = os.path.join(path, file)          
-        sheet1.write(i,0,source_filename)
+        sheet1.cell(i,1,source_filename)
         
         destination_filename = os.path.join(destinationFolder, os.path.relpath(path,sourceFolder))
         destination_filename = os.path.join(destination_filename,file)
@@ -79,38 +84,38 @@ for (path, dirs, files) in os.walk(sourceFolder):
         
         if(os.path.isfile(destination_filename)):    
 
-            sheet1.write(i,2,destination_filename)   
+            sheet1.cell(i,3,destination_filename)   
 
             folder_size_destination += os.path.getsize(destination_filename)    
 
             SourceCheckSum = md5Checksum(source_filename)
-            sheet1.write(i,1,SourceCheckSum)   
+            sheet1.cell(i,2,SourceCheckSum)   
             
             DestinationCheckSum = md5Checksum(destination_filename)
-            sheet1.write(i,3,DestinationCheckSum)   
+            sheet1.cell(i,4,DestinationCheckSum)   
             
             #Compare the checksum of Source and Files
             if(SourceCheckSum != DestinationCheckSum):
-                sheet1.write(i,4,'Checksum does not match')
+                sheet1.cell(i,5,'Checksum does not match')
             else:
-                sheet1.write(i,4,'Checksum matched')
+                sheet1.cell(i,5,'Checksum matched')
                       
         else:
-            sheet1.write(i,4,destination_filename + ' does not exist')            
+            sheet1.cell(i,5,destination_filename + ' does not exist')            
         
         i = i+1
             
 i=i+3
-sheet1.write(i,0,'Source Folder Size',style0)
-sheet1.write(i,1,'%0.1f MB' % (folder_size_source/(1024*1024.0)))
+sheet1.cell(i,1,'Size of files found in source')
+sheet1.cell(i,2,'%0.1f MB' % (folder_size_source/(1024*1024.0)))
 
 i=i+1
-sheet1.write(i,0,'Destination Folder Size',style0)
-sheet1.write(i,1,'%0.1f MB' % (folder_size_destination/(1024*1024.0)))        
+sheet1.cell(i,1,'Size of files found in destination')
+sheet1.cell(i,2,'%0.1f MB' % (folder_size_destination/(1024*1024.0)))        
    
 print('')
 print("Source Folder Size: %0.1f MB" % (folder_size_source/(1024*1024.0)))
 print("Destination Folder Size = %0.1f MB" % (folder_size_destination/(1024*1024.0)))
 
-book.save('Result.xls')
+book.save('Result.xlsx')
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# FileComparison" 
+# File Comparison
+This simple script allows you to specify a source and a destination folder and it compares all files recursively within them.
+The result is an excel file with the details of the comparison.
+
+Each file from the source is checked to see if a corresponding file with the same name exists in the destination.
+If found, both files are compared by generating an MD5 for each of them.
+
+Dependencies: hashlib,openpyxl
